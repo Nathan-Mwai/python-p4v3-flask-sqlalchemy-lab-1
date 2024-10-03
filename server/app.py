1cca255f07be700ea6a8e5fe4f2f96f83a23ceb8
Nathan Mwai <nathan.mwai@student.moringaschool.com>
2024-10-03 08:04:20 +0300

    finishes lab

--- server/app.py
+++ server/app.py
@@ -21,7 +21,49 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route('/earthquakes/<int:id>')
+def earthquakes(id):
+    # earthquake = [{
+    #     "id": quake.id,
+    #     "location": quake.location,
+    #     "magnitude": quake.magnitude,
+    #     "year": quake.year
+    # } for quake in Earthquake.query.filter(Earthquake.id == id)]
+    # if earthquake:
+    #     return make_response(earthquake, 200)
+    # else:
+    #     return {
+    #         "message": f"Earthquake {id} not found."
+    #     }
+    # PYTEST REFUSED FIRST OPTION
+    earthquake = db.session.get(Earthquake, id)
+    if earthquake:
+        response = make_response(
+            {
+                "id": earthquake.id,
+                "location": earthquake.location,
+                "magnitude": earthquake.magnitude,
+                "year": earthquake.year,
+            },
+            200,
+        )
+    else:
+        response = make_response({"message": f"Earthquake {id} not found."}, 404)
+    return response
 
-
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def find_magnitudes(magnitude):
+    magnitudes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+    response = {
+        "count": len(magnitudes),
+        "quakes": [{
+            "id": eqs.id,
+            "location": eqs.location,
+            "magnitude": eqs.magnitude,
+            "year": eqs.year
+        } for eqs in magnitudes]
+    }
+    return make_response(response,200)
+        
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
